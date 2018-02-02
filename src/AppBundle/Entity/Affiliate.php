ce23e708a0c75ba2f6271854294ab2c74e5db265
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2018-02-02 15:38:30 +0200

    Entities :: annotations

--- src/AppBundle/Entity/Affiliate.php
+++ src/AppBundle/Entity/Affiliate.php
@@ -1,21 +1,52 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ms
- * Date: 2.02.18
- * Time: 14:29
- */
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="affiliate")
+ * @ORM\HasLifecycleCallbacks()
+ */
 
 class Affiliate
 {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     private $id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Category", inversedBy="affiliates")
+     * @ORM\JoinTable(name="affiliate_categories")
+     */
     private $categories;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
     private $url;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
     private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     */
     private $token;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
     private $isActive;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
     private $createdAt;
 }
