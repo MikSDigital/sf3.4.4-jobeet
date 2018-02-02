b49cf4fe4f36b5b22bb9acaf4265267da4a144b9
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2018-02-02 14:31:02 +0200

    Entities

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Jobeet, Symfony Standard Edition
-========================
-
+Jobeet, Symfony Standard Edition (3.4.4)
+========================================
+Follow by 
 
 https://medium.com/@dragosholban/symfony-2-8-jobeet-day-1-starting-up-the-project-9e9c2e70c6ab
 
---
+++ src/AppBundle/Entity/Affiliate.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ms
+ * Date: 2.02.18
+ * Time: 14:29
+ */
+
+namespace AppBundle\Entity;
+
+
+class Affiliate
+{
+    private $id;
+    private $categories;
+    private $url;
+    private $email;
+    private $token;
+    private $isActive;
+    private $createdAt;
+}
